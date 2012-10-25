f03d53a04a2966701f9ad1b816ddf9d144c9cbe3
Nilay Binjola <nilaybinjola@gmail.com>
2012-10-25 19:29:50 +0530

    Simulator: Aesthetics Improved.  - Background Image changed.  - Hover Effects on Navigation Bar Links removed.  - Hover Effect on Banner Title removed.

--- showContent.js
+++ showContent.js
@@ -2,8 +2,11 @@
 
 
 
-var about_text = "The sudden rush of blood to your head wakes you up with a kick. The bright lights push you forward. The crazed crowd pulls you further. You look around yourself in a daze. You're on a stage that most others only dream of. This is Rocktaves 2011.</p>p>Rocktaves is the rock festival of BITS Pilani. It is without a doubt one of the most prestigious rock competitions in India. Rocktaves is a major event of the cultural festival Oasis, which also includes other events such as a solo guitar contest called Axetacy, and an acoustic unplugged event called Oasis Unplugged.</p><p>For decades, Rocktaves has enjoyed its lofty position as one of the most venerated of all platforms in the fairly nascent Indian rock scene. Its claims to fame are numerous. Rocktaves is in factresponsible for launching some of the biggest names in the Indian rock scene today: Parikrama, Prestorika, Them Clones, The Superfuzz and Euphoria are just some of those names.</p><p>You could be next.</p>"
+var about_text = "<p>Rocktaves is the annual semi-professional rock festival of BITS Pilani. It is without a doubt one of the most prestigious rock competitions in India. Rocktaves is a major event of the cultural festival Oasis, which also includes other musical events such as the solo guitar contest called \"Axetacy\", and the acoustic unplugged event called \"Oasis Unplugged\".</p><p>For decades, Rocktaves has strived towards excellence, pioneering and redefining the fairly young and volatile Indian rock scene. Rocktaves is, in fact, responsible for launching some of the biggest names in the Indian rock scene today; Parikrama, Prestorika, Them Clones, The Superfuzz and Euphoria are just some of the footnotes in this epic musical odyssey. It just might be your turn now...</p>";
 
+$(document).ready(function(){
+
+});
 
 function showContent(contentID) 
 {
